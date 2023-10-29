19bad14001262eaaf33c34914797709ea6128294
Catherine Safiullina <38792316+Harunatsuko@users.noreply.github.com>
2023-10-29 07:37:29 +0500

    test version

--- tracking/tracking.py
+++ tracking/tracking.py
@@ -1,9 +1,13 @@
-import os
-import numpy as np
 import cv2
+import numpy as np
 from dataclasses import dataclass
+import pandas as pd
+import os
 import matplotlib.pyplot as plt
-from utils import preprocess
+from PIL import Image
+from sklearn.linear_model import LinearRegression
+import onnxruntime as ort
+from plot_utils import cells_to_bboxes, plot_image, non_max_suppression
 
 @dataclass
 class RecyclableObject:
@@ -12,28 +16,26 @@ class RecyclableObject:
     next_pos: list
     ch_mean: np.array
 
-def custom_distance(bbox, ch_mean, obj, coeffs = [0.3, 0.2, 0.2, 0.3]):
+def custom_distance(bbox, ch_mean, obj):
     ## сравниваем по размеру bbox
     ## сравниваем по среднему каналов
     ## сравниваем по расстоянию между центрами масс
     ## сравниваем по предсказанному центру масс и реальному
     bbox_area = bbox[3]*bbox[4]
     obj_bbox_area = obj.bbox[3]*obj.bbox[4]
-    area_diff = min(bbox_area, obj_bbox_area) / max(bbox_area, obj_bbox_area)
-    
+    #0
+    area_diff = 1 - min(bbox_area, obj_bbox_area) / max(bbox_area, obj_bbox_area)
+    #1
     ch_diff = np.abs(ch_mean-obj.ch_mean).mean()
-    
+    #2
     distance = np.linalg.norm(np.array([bbox[1], bbox[2]])-np.array([[obj.bbox[1], obj.bbox[2]]]))
-    
+    #3
     if obj.next_pos is not None:
         pred_distance = np.linalg.norm(np.array([bbox[1], bbox[2]])-obj.next_pos)
     else:
-        pred_distance = 0
-        correct_val = coeffs[-1]/3
-        coeffs = [coeff+correct_val for coeff in coeffs]
-        coeffs[-1] = 0
+        pred_distance = None
     
-    return coeffs[0]*area_diff+coeffs[1]*ch_diff+coeffs[2]*distance+coeffs[3]*pred_distance
+    return area_diff, ch_diff, distance, pred_distance
 
 
 class LinearPredictor():
@@ -81,10 +83,43 @@ class LinearPredictor():
     def start_learn(self):
         self._learn = True
         
+class Visualiser():
+    def __init__(self, class_colors=None):
+        if class_colors is None:
+            class_colors = {0:(255,0,0),
+                            1:(0,255,0),
+                            2:(0,0,255),
+                            3:(150,0,150)}
+        self.class_colors = class_colors
         
+    def draw_bbox_rgb(self, frame, objects):
+        frame = np.array(frame)
+        for obj in objects:
+            class_label = obj.bbox[0]
+            top, bot = from_yolo_to_cv(obj.bbox, frame.shape)
+            frame = cv2.rectangle(frame, top, bot, self.class_colors[class_label], 2)
+            frame = cv2.putText(frame, str(obj.idx), [top[0], bot[1]],
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, self.class_colors[class_label],2)
+        return frame
+    
+    def draw_tif(self, frame, objects=None, channel_num=11):
+        gray_imgs = []
+        for channel in range(channel_num):
+            frame.seek(channel)
+            gray_img = np.uint8(np.array(frame)*255)
+            if objects is not None:
+                for obj in objects:
+                    class_label = obj.bbox
+                    top, bot = from_yolo_to_cv(obj.bbox)
+                    gray_img = cv2.rectangle(gray_img, top, bot,self.class_colors[class_label], 2)
+                    gray_img = cv2.putText(gray_img, str(obj.idx), [top[0], bot[1]],
+                                    cv2.FONT_HERSHEY_SIMPLEX, 1, self.class_colors[class_label],2)
+            gray_imgs.append(gray_img)
+        return gray_imgs
+    
 class Traker():
     def __init__(self,
-                 detector_path = '',
+                 detector_path,
                  stay_frames_thresh=3,
                  min_dist_threshold = 0.01,
                  max_dist_threshold = 0.3,
@@ -97,29 +132,31 @@ class Traker():
         self.stay_frames_thresh = stay_frames_thresh
         self._current_objs = []
         self._curr_id = 0
-#         self.detector = self._load_detector(detector_path)
+        self.detector_path = detector_path
         self.is_staying = False
         self.stay_frames = 0
         self.linear_predictor = LinearPredictor()
         self.visualiser = Visualiser()
+        self.session = ort.InferenceSession(self.detector_path)
     
     def _get_channel_means(self, bboxes, arr):
         ch_means = []
         for bbox in bboxes:
-            top, bot = from_yolo_to_cv(bbox, arr.shape[1:3])
-            bbox_arr = arr[:,top[0]:bot[0], top[1]:bot[1]]
+            top, bot = from_yolo_to_cv(bbox, (arr.shape[2],arr.shape[1]))
+            
+            bbox_arr = arr[:, top[0]:bot[0], top[1]:bot[1]]
             means = np.array([np.mean(bbox_arr[i, :, :]) for i in range(len(bbox_arr))])
             ch_means.append(means)
         return ch_means
             
     
-    def track(self, frame_rgb, frame_ms, frame_idx):
-        # TODO: test stage
-        bboxes = self._detect(frame_rgb, frame_ms, frame_idx)
+    def track(self, frame_rgb, frame_ms, frame_idx, path_rgb):
         arr = preprocess(frame_rgb, frame_ms)
+        bboxes = self._detect(arr)
+        
         bboxes_channel_means = self._get_channel_means(bboxes,
                                                        arr)
-        objs, is_stay_detected = self._update_tracks(bboxes, bboxes_channel_means)
+        objs, is_stay_detected = self._update_tracks(bboxes, bboxes_channel_means, arr)
         self._stay_check(is_stay_detected)
         self._current_objs = objs
         if self.is_draw_bbox_rgb:
@@ -147,90 +184,86 @@ class Traker():
                 self.stay_frames = 0
                 self.linear_predictor.start_learn()
         
-    def _update_tracks(self, bboxes, bboxes_channel_means):
+    def _update_tracks(self, bboxes, bboxes_channel_means, arr):
         stay_boxes = 0
+        x_left_border = 0.98
+        x_right_border = 0.02
         objs = []
         busy_obj_idxs = []
         for bbox, ch_mean in zip(bboxes, bboxes_channel_means):
-#             print(bbox)
-            distance = [(obj, custom_distance(bbox,ch_mean, obj)) for obj in self._current_objs \
-                        if obj.idx not in busy_obj_idxs]
-            if len(distance):
-                distance = min(distance, key = lambda d: d[1])
-                print('Distance', distance[1])
-                if distance[1] > self.max_dist_threshold:
-                    print('Found new object!')
+            top, bottom = from_yolo_to_cv(bbox)
+            if bottom[0] < x_left_border and top[0] > x_right_border:
+                good_cand = []
+                metrices = []
+                for obj in self._current_objs:
+                    area_diff, ch_diff, distance, pred_distance = custom_distance(bbox,ch_mean, obj)
+                    if pred_distance is not None:
+                        if pred_distance < 0.1:
+                            if area_diff < 0.15 and ch_diff < 10 and distance < 0.2:
+                                good_cand.append(obj)
+                                metrices.append((distance+pred_distance)/2)
+                                
+                    else:
+                        if area_diff < 0.1 and ch_diff < 10 and distance < 0.08:
+                            good_cand.append(obj)
+                            metrices.append(distance)
+                                
+                if len(good_cand) > 1:
+                    max_m = 100
+                    best_cand = None
+                    for cand, m in zip(good_cand, metrices):
+                        if m < max_m:
+                            max_m = m
+                            best_cand = cand
+                            objs.append(RecyclableObject(bbox,
+                                         best_cand.idx,
+                                         self.linear_predictor.predict_next_pos(bbox,best_cand.idx),
+                                         ch_mean))
+                            if max_m < self.min_dist_threshold:
+                                stay_boxes += 1
+                elif len(good_cand) == 1:
+                    best_cand = good_cand[0]
+                    
                     objs.append(RecyclableObject(bbox,
-                                     self._curr_id,
-                                     self.linear_predictor.predict_next_pos(bbox,self._curr_id),
-                                     ch_mean))
-                    self._curr_id += 1
-                else:
-                    objs.append(RecyclableObject(bbox,
-                                     distance[0].idx,
-                                     self.linear_predictor.predict_next_pos(bbox,distance[0].idx),
-                                     ch_mean))
-                    busy_obj_idxs.append(distance[0].idx)
-                    if distance[1] < self.min_dist_threshold:
+                                         best_cand.idx,
+                                         self.linear_predictor.predict_next_pos(bbox,best_cand.idx),
+                                         ch_mean))
+                    if metrices[0] < self.min_dist_threshold:
                         stay_boxes += 1
-            else:
-                print('No object to compare')
-                objs.append(RecyclableObject(bbox,
-                                     self._curr_id,
-                                     self.linear_predictor.predict_next_pos(bbox,self._curr_id),
-                                     ch_mean))
-                self._curr_id += 1
-                
+                else:
+                    is_real_object = self.check_bbox_ch_means(bbox, arr, top, bottom)
+                    if is_real_object:
+                        objs.append(RecyclableObject(bbox,
+                                         self._curr_id,
+                                         self.linear_predictor.predict_next_pos(bbox,self._curr_id),
+                                         ch_mean))
+                        self._curr_id += 1
         return objs, stay_boxes == len(bboxes)
+    
+    
+    def check_bbox_ch_means(self, bbox, arr, top, bot):
+        w, h = arr.shape[0], arr.shape[1]
+        bbox_left = [bbox[0],max(0,bbox[1]-0.1),bbox[2], bbox[3], bbox[4]]
+        bbox_right = [bbox[0],min(w,bbox[1]+0.1),bbox[2], bbox[3], bbox[4]]
+        ch_means = self._get_channel_means([bbox_left,bbox_right,bbox], arr)
+        c1 = (np.mean(ch_means[0]-ch_means[1]) - np.mean(ch_means[0]-ch_means[2])) > 5
+        c2 = (np.mean(ch_means[0]-ch_means[1]) - np.mean(ch_means[1]-ch_means[2])) > 5
+        return c1 and c2
                 
-                
-    def _detect(self, _frame_rgb, _frame_ms, frame_idx):
-        bboxes = [[np.random.randint(4), 0.2, 0.1, 0.3, 0.1],
-                  [np.random.randint(4), 0.3, 0.2, 0.4, 0.1]]
-        return bboxes
-    
-class Visualiser():
-    def __init__(self, class_colors=None):
-        if class_colors is None:
-            class_colors = {0:(255,0,0),
-                            1:(0,255,0),
-                            2:(0,0,255),
-                            3:(150,0,150)}
-        self.class_colors = class_colors
-        
-    def draw_bbox_rgb(self, frame, objects):
-        frame = np.array(frame)
-        for obj in objects:
-            print(obj)
-            class_label = obj.bbox[0]
-            top, bot = from_yolo_to_cv(obj.bbox, frame.shape)
-            frame = cv2.rectangle(frame, top, bot, self.class_colors[class_label], 2)
-            frame = cv2.putText(frame, str(obj.idx), [top[0], bot[1]],
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, self.class_colors[class_label],2)
-        return frame
-    
-    def draw_tif(self, frame, objects=None, channel_num=11):
-        gray_imgs = []
-        for channel in range(channel_num):
-            frame.seek(channel)
-            gray_img = np.uint8(np.array(frame)*255)
-            if objects is not None:
-                for obj in objects:
-                    class_label = obj.bbox
-                    top, bot = from_yolo_to_cv(obj.bbox)
-                    gray_img = cv2.rectangle(gray_img, top, bot,self.class_colors[class_label], 2)
-                    gray_img = cv2.putText(gray_img, str(obj.idx), [top[0], bot[1]],
-                                    cv2.FONT_HERSHEY_SIMPLEX, 1, self.class_colors[class_label],2)
-            gray_imgs.append(gray_img)
-        return gray_imgs
+    def _detect(self, arr, thr=0.9):
+        input_arr = arr/255
+        outputs = self.session.run(None, {"input.1": np.float32([input_arr])})
+        bboxes = cells_to_bboxes(outputs, is_pred=True, to_list=False)
+        bboxes = non_max_suppression(bboxes, iou_threshold=0.01, threshold=thr, tolist=False)
+        bboxes = bboxes.numpy()
+        boxes_out = [[bbox[0]]+list(bbox[2:]/640) for bbox in bboxes]
+        return boxes_out
     
     
+# Usage
+# traker = Traker('yolov5_v6_640_640.onnx')
 # for i, framep in enumerate(frames_pths):
 #     frame_rgb = Image.open(framep)
 #     frame_ms = Image.open(framep.replace('frames_rgb',
 #                                 'frames_ms').replace('png','tif'))
-#     objs, rgb_img, gray_imgs = traker.track(frame_rgb,frame_ms,i)
-#     fig, ax = plt.subplots(figsize=(7,7))
-    
-#     ax.imshow(rgb_img)
-#     plt.show()
+#     objs, rgb_img, gray_imgs = traker.track(frame_rgb,frame_ms,i, framep )
